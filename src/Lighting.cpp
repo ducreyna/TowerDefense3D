a30a6b8228e53efb02465dd87b6121d20a6c9bb1
ducreyna <ducrey.nathan@gmail.com>
2012-12-06 14:08:13 +0100

    creation de la classe gerant la lumiere

---
+++ src/Lighting.cpp
@@ -0,0 +1,18 @@
+/*
+ * Lighting.cpp
+ *
+ *  Created on: 6 déc. 2012
+ *      Author: nathan
+ */
+
+#include "Lighting.h"
+
+Lighting::Lighting() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Lighting::~Lighting() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/Lighting.h
@@ -0,0 +1,17 @@
+/*
+ * Lighting.h
+ *
+ *  Created on: 6 déc. 2012
+ *      Author: nathan
+ */
+
+#ifndef LIGHTING_H_
+#define LIGHTING_H_
+
+class Lighting {
+public:
+	Lighting();
+	virtual ~Lighting();
+};
+
+#endif /* LIGHTING_H_ */
