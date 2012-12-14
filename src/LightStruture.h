2ebaab3db27c3d5d9cb9a6ef691976d8231046aa
ducreyna <ducrey.nathan@gmail.com>
2012-12-14 21:22:59 +0100

    création de la structure pour l'éclairage

---
+++ src/LightStruture.h
@@ -0,0 +1,48 @@
+/*
+ * test.h
+ *
+ *  Created on: 13 déc. 2012
+ *      Author: nathan
+ */
+
+#ifndef LIGHTSTRUCTURE_H_
+#define LIGHTSTRUCTURE_H_
+
+/* ############################## */
+/* LIGHT STRUCTURE */
+struct COORD
+{
+	GLdouble x;
+	GLdouble y;
+	GLdouble z;
+	GLdouble w;
+};
+
+struct RGB
+{
+	GLdouble r;     /* rouge */
+	GLdouble g;     /* vert */
+	GLdouble b;     /* bleu */
+};
+
+struct ALLURE_FAISCEAU
+{
+        GLdouble k;
+        GLdouble theta;
+};
+
+
+struct LIGHT_SOURCE
+{
+	RGB             ambiant;         /* couleur "ambiente" */
+	RGB             color;          /* couleur de la source */
+	COORD           position;         /* position de la source */
+	ALLURE_FAISCEAU allure_faisceau;  /* allure du faisceau lumineux */
+	COORD           direction;        /* direction du faisceau lumineux */
+	GLint		on;
+};
+/* ############################## */
+
+
+
+#endif /* LIGHTSTRUCTURE_H_ */
